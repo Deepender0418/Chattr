6e9a14613d12859a313ee824de5dbbff4fdfff1b
Deepender0418 <101545760+Deepender0418@users.noreply.github.com>
2025-10-09 22:25:53 +0530

    Update message_route.js

--- Backend/src/routes/message_route.js
+++ Backend/src/routes/message_route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import protectRoute from '../middleware/auth_middleware.js';
-import { getFriends, getMessages, sendMessage } from '../controllers/message_controller.js';
+import { getUsersForSidebar, getMessages, sendMessage } from '../controllers/message_controller.js';
 
 const router = express.Router();
 
-router.get("/users", protectRoute, getFriends);
+router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:msgToId", protectRoute, getMessages);
 router.post("/send/:id", protectRoute, sendMessage);
+
 
 export default router;
