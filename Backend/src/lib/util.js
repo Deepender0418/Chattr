c80e9098728fd361ca644448d7c33baff32d3ec5
Deepender0418 <101545760+Deepender0418@users.noreply.github.com>
2025-10-09 17:36:32 +0530

    Update util.js

--- Backend/src/lib/util.js
+++ Backend/src/lib/util.js
@@ -8,8 +8,8 @@ export const generateToken = (userId, res) => {
         secure: process.env.NODE_ENV === 'production',
         sameSite: 'Strict',//CSRF protection
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-        secure: process.env.NODE_ENV==='production'
     });
 
     return token;
 }
+
