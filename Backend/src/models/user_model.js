04858779dbbe32c3340109819b20d7541ff62d3b
Deepender0418 <101545760+Deepender0418@users.noreply.github.com>
2025-10-09 22:14:41 +0530

    Update user_model.js

--- Backend/src/models/user_model.js
+++ Backend/src/models/user_model.js
@@ -1,11 +1,34 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    fullname: { type: String, required: true },
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, minlength: 6 },
-    profilePicture: { type: String, default: "" },
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        fullName: {
+            type: String,
+            required: true,
+        },
+        userName: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+        },
+        profilePic: {
+            type: String,
+            default: "https://i.pinimg.com/1200x/56/af/7e/56af7ed2c15a58fed21e8ffd0744bb1e.jpg",
+        },
+    },
+    { timestamps: true }
+);
 
-export const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+
+export default User;
