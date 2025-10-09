33bad7ee10ee468f9f8bddd424fc66d8141b67cc
Deepender0418 <101545760+Deepender0418@users.noreply.github.com>
2025-10-09 17:28:25 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -14,10 +14,15 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 
+app.use("/api/health", (req, res) => {
+    res.status(200).send("API is running");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
     connectDB();
+
 });
