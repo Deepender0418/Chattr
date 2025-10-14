cf06714076bc4fbfd61937418458ead8624c14fd
Deepender0418 <101545760+Deepender0418@users.noreply.github.com>
2025-10-14 22:19:28 +0530

    Update axios.js

--- Web/src/lib/axios.js
+++ Web/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.BACKENDURL,
+    baseURL: `${import.meta.env.BACKENDURL}/api`,
     withCredentials: true,
 });
