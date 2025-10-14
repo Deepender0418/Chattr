fb63830578445a7512c080db31b7c2f43f0de2dc
Deepender0418 <101545760+Deepender0418@users.noreply.github.com>
2025-10-14 22:14:48 +0530

    Update useAuthStore.js

--- Web/src/store/useAuthStore.js
+++ Web/src/store/useAuthStore.js
@@ -3,8 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-// const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
-const BASE_URL = "https://chattr-backend-5hv9.onrender.com";
+const BASE_URL = import.meta.env.BACKENDURL;
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
