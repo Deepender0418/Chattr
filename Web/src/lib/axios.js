311d96b085f0b600481ad7fc25de8bac3e6e61b7
Deepender0418 <101545760+Deepender0418@users.noreply.github.com>
2025-10-14 23:51:16 +0530

    Update axios.js

--- Web/src/lib/axios.js
+++ Web/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: `${import.meta.env.BACKENDURL}/api`,
+    // baseURL: `${import.meta.env.BACKENDURL}/api`,
+    baseURL: "https://chattr-backend-5hv9.onrender.com/api",
     withCredentials: true,
 });
