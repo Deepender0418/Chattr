06faa102ba440a0a09a98648e1f9655b0282f860
Deepender0418 <101545760+Deepender0418@users.noreply.github.com>
2025-10-15 02:42:30 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,9 +1,7 @@
 import express from "express";
-import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
-// import path from "path";
+import dotenv from "dotenv";
 
 import { connectDB } from "./lib/db.js";
 
@@ -12,15 +10,15 @@ import messageRoutes from "./routes/message_route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
+const FrontURL = process.env.FRONTENDURL;
 
 const PORT = process.env.PORT;
-// const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "https://chatttr.onrender.com",
+        origin: FrontURL,
         credentials: true,
     })
 );
@@ -31,15 +29,6 @@ app.use("/api/health", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-//     app.get("*", (req, res) => {
-//         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//     });
-// }
-
 server.listen(PORT, () => {
-    console.log("server is running on PORT:" + PORT);
     connectDB();
 });
