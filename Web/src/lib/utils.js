4927453ab5b5afa14cfd010bc69b3616dd923434
Deepender0418 <101545760+Deepender0418@users.noreply.github.com>
2025-10-18 15:52:23 +0530

    Delete Web directory

--- Web/src/lib/utils.js
+++
@@ -1,7 +0,0 @@
-export function formatMessageTime(date) {
-    return new Date(date).toLocaleTimeString("en-IN", {
-        hour: "numeric",
-        minute: "2-digit",
-        hour12: true,
-    });
-}
