e41eb72f81698c393c8e8607a6a726b2fca1e8e8
Deepender0418 <101545760+Deepender0418@users.noreply.github.com>
2025-10-20 03:36:47 +0530

    Update App.jsx

--- Web/src/App.jsx
+++ Web/src/App.jsx
@@ -39,6 +39,7 @@ const App = () => {
       <Navbar />
 
       <Routes>
+        <Route path="*" element={<Navigate to="/" />} />
         <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
         <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
         <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
