ba798cebd68d98a41f9b744ca77d6dccd7afac9a
Deepender Singh <101545760+Deepender0418@users.noreply.github.com>
2025-12-09 15:25:26 +0530

    Implement email verification page component

---
+++ Web/src/pages/VerifyEmailPage.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import { useParams, Navigate, Link } from "react-router-dom";
+import { Loader } from "lucide-react";
+import { useAuthStore } from "../store/useAuthStore";
+import toast from "react-hot-toast";
+
+export default function VerifyEmailPage() {
+    const { token } = useParams();
+    const verifyEmailLogin = useAuthStore((s) => s.verifyEmailLogin);
+
+    const [status, setStatus] = useState("loading");
+
+    useEffect(() => {
+        const verify = async () => {
+        try {
+            const result = await verifyEmailLogin(token);
+
+            if (result?.success) {
+            toast.success(result.message || "Email verified successfully!");
+            setStatus("success");
+            return;
+            }
+
+            setStatus("error");
+        } catch (err) {
+            setStatus("error");
+        }
+        };
+
+        verify();
+    }, [token, verifyEmailLogin]);
+
+    if (status === "loading") {
+        return (
+        <div className="flex flex-col items-center justify-center h-screen space-y-4">
+            <Loader className="size-12 animate-spin text-primary" />
+            <p className="text-lg font-semibold">Verifying your email...</p>
+        </div>
+        );
+    }
+    const verified = () => {
+    if (status === "success") {
+        // return <Navigate to="/" replace />;
+        return (
+            <div className="flex flex-col items-center justify-center h-screen text-center space-y-4">
+                <h1 className="text-2xl font-bold text-red-500">Verified Successfully!!!</h1>
+                <Link to="/" className="btn btn-primary mt-4">
+                    Go to HomePage
+                </Link>
+            </div>
+        )
+    }
+    else
+    {
+        return(
+        <div className="flex flex-col items-center justify-center h-screen text-center space-y-4">
+        <h1 className="text-2xl font-bold text-red-500">Verification Failed ❌</h1>
+        <p className="text-base-content/70 max-w-md">
+            The verification link is invalid or expired.
+        </p>
+        <Link to="/login" className="btn btn-primary mt-4">
+            Go to Login
+        </Link>
+        </div>
+        )
+    }
+    }
+    return (
+        verified()
+    );
+}
