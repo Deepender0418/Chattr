be95d71dd3e9fdc584869e18274314b15addaf31
Deepender Singh <101545760+Deepender0418@users.noreply.github.com>
2025-12-10 22:01:36 +0530

    Add route to mark messages as seen

--- Backend/src/routes/message_route.js
+++ Backend/src/routes/message_route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth_middleware.js";
-import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message_controller.js";
+import { getMessages, getUsersForSidebar, sendMessage, markMessagesAsSeen } from "../controllers/message_controller.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
 router.post("/send/:id", protectRoute, sendMessage);
+router.post("/:id/mark-seen", protectRoute, markMessagesAsSeen);
 
 export default router;
